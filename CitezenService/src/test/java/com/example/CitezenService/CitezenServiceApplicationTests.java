46f9b5f7b089af7de3231fbbfb2ac47c07fdef9f
Chinnasowry <dasarichinnasowry@gmail.com>
2023-10-30 22:21:54 +0530

    Citezen Service Created

---
+++ CitezenService/src/test/java/com/example/CitezenService/CitezenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CitezenService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitezenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

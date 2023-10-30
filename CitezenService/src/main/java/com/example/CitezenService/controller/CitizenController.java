61e9ac4c3aa0e0529aec3fda17d7ea6704b508c0
Chinnasowry <dasarichinnasowry@gmail.com>
2023-10-30 23:25:07 +0530

    controller and entity files added

--- CitezenService/src/main/java/com/example/CitezenService/controller/CitizenController.java
+++ CitezenService/src/main/java/com/example/CitezenService/controller/CitizenController.java
@@ -1,2 +1,18 @@
-package com.example.CitezenService.controller;public class CitizenController {
+package com.example.CitezenService.controller;
+
+
+import com.example.CitezenService.entity.Citizen;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("citizen")
+public class CitizenController {
+
+    @GetMapping("get")
+    public ResponseEntity<Citizen> getCitizen(){
+        return null;
+    }
 }

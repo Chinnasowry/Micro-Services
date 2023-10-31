00eb9302278e23a461af2ea3203820656abce552
Chinnasowry <dasarichinnasowry@gmail.com>
2023-10-31 11:35:06 +0530

    Vaccination center created

---
+++ VaccinationCenterService/src/test/java/com/example/VaccinationCenterService/VaccinationCenterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VaccinationCenterService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationCenterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
